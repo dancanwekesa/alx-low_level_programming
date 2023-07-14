a17c61db17b7e02b6acc22ee7e6305581645d288
dancanwekesa <dancanwekesa@gmail.com>
2023-07-14 00:50:06 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#index <stdio.h>
+#include <stdio.h>
 /**
- * main - A program that print a line using the printf function
- * Return: 0 (Sucess)
+ * main - A program that prints a line using printf
+ * Return: 0 (Successful)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

69abb39cce95680f8a3ddd82d469597ab9e6de90
dancanwekesa <dancanwekesa@gmail.com>
2023-07-19 08:03:36 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,11 @@
 /**
  * print_alphabet - print all alphabet in lowercase
  *
- * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-int i;
-
-for (i = 97; i = 122; i++)
-{
-_putchar(i)
-}
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 _putchar('\n');
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,48 @@
 #include "main.h"
+
+
 /**
- * print_sign - prints sign of a number
- * @n: number to be checked
- * Return: 1 if n is greater than 0,-1 ifnn is less than 0, else 0
+
+ * print_sign - prints the sign of a number
+
+ * @n: the int to check
+
+ * Return: 1 and prints + if n is greater than zero
+
+ * 0 and prints 0 if n is zero
+
+ * -1 and prints - if n is less than zero
+
  */
+
 int print_sign(int n)
+
 {
+
         if (n > 0)
+
         {
+
                 _putchar('+');
+
                 return (1);
-        }
-        if (n == 0)
-        {
-                _putchar('0');
+
+        } else if (n == 0)
+
+}
+
+                _putchar(48);
+
                 return (0);
+
+        } else if (n < 0)
+
+        {
+
+                _putchar('-');
+
         }
-        _putchar('-');
-        return (-1);
+
+                return (-1);
+
 }
